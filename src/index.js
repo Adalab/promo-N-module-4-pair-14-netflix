cec92b5d5a825f18c66142322a1219f1e56b0e0d
Paloma <paloma.arevalog@gmail.com>
2021-10-12 20:05:19 +0200

    express II

--- src/index.js
+++ src/index.js
@@ -9,5 +9,26 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+	console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+	const response = {
+		success: true,
+		movies: [
+			{
+				id: '1',
+				title: 'Gambita de dama',
+				gender: 'Drama',
+				image: 'https://via.placeholder.com/150',
+			},
+			{
+				id: '2',
+				title: 'Friends',
+				gender: 'Comedia',
+				image: 'https://via.placeholder.com/150',
+			},
+		],
+	};
+	res.json(response);
 });
