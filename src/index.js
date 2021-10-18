8274ac0251211fea9dfc9abd1c3430bee3aa5959
sasalinero <sasalinero@gmail.com>
2021-10-18 11:24:26 +0200

    new users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const Database = require('better-sqlite3');
-const db = new Database('./src/db/database.db', { verbose: console.log });
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const Database = require("better-sqlite3");
+const db = new Database("./src/db/database.db", { verbose: console.log });
 
 // create and config server
 const server = express();
@@ -12,41 +12,31 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4001;
 server.listen(serverPort, () => {
-	console.log(`Server listening at http://localhost:${serverPort}`);
+  console.log(`Server listening at http://localhost:${serverPort}`);
 });
-server.get('/movie/:movieId', (req, res) => {
-	console.log('URL params', req.params);
-	const foundMovie = movies.find((movie) => movie.id === req.params.moviesId);
-	console.log('Found movie:', foundMovie);
+server.get("/movie/:movieId", (req, res) => {
+  console.log("URL params", req.params);
+  const foundMovie = movies.find((movie) => movie.id === req.params.moviesId);
+  console.log("Found movie:", foundMovie);
 });
 
-server.get('/movies', (req, res) => {
-	// preparo la query
-	const query = db.prepare('SELECT * FROM movies');
-	// ejecuto la query para obtener todos los registros en un array
-	const movies = query.all();
+server.get("/movies", (req, res) => {
+  // preparo la query
+  const query = db.prepare("SELECT * FROM movies");
+  // ejecuto la query para obtener todos los registros en un array
+  const movies = query.all();
 
-	/* 	const response = {
-		success: true,
-		movies: [
-			{
-				id: '1',
-				title: 'Gambita de dama',
-				gender: 'Drama',
-				image: 'https://via.placeholder.com/150',
-			},
-			{
-				id: '2',
-				title: 'Friends',
-				gender: 'Comedia',
-				image: 'https://via.placeholder.com/150',
-			},
-		],
-	}; */
-	res.json({
-		success: true,
-		movies: movies,
-	});
+  res.json({
+    success: true,
+    movies: movies,
+  });
 });
-const staticServerPathWeb = './src/public-react/'; // En esta carpeta ponemos los ficheros estáticos
+
+server.post("/signup", (req, res) => {
+  const query = db.prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+  const result = query.run(req.body.email, req.body.password);
+  console.log(result);
+  res.json(result);
+});
+const staticServerPathWeb = "./src/public-react/"; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
