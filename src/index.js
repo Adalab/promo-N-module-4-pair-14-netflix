a5a37dbc70fff36e7a4dc7624f60aaeae0196586
sasalinero <sasalinero@gmail.com>
2021-10-20 09:44:18 +0200

    add coments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const Database = require('better-sqlite3');
-const db = new Database('./src/db/database.db', { verbose: console.log });
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const Database = require("better-sqlite3");
+const db = new Database("./src/db/database.db", { verbose: console.log });
 
 // create and config server
 const server = express();
@@ -12,47 +12,45 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4001;
 server.listen(serverPort, () => {
-	console.log(`Server listening at http://localhost:${serverPort}`);
+  console.log(`Server listening at http://localhost:${serverPort}`);
 });
-server.get('/movie/:movieId', (req, res) => {
-	console.log('URL params', req.params);
-	const foundMovie = movies.find((movie) => movie.id === req.params.moviesId);
-	console.log('Found movie:', foundMovie);
+server.get("/movie/:movieId", (req, res) => {
+  console.log("URL params", req.params);
+  const foundMovie = movies.find((movie) => movie.id === req.params.moviesId);
+  console.log("Found movie:", foundMovie);
+}); //Esto es para encontrar una pelicula
+
+server.get("/movies", (req, res) => {
+  // preparo la query
+  const query = db.prepare("SELECT * FROM movies");
+  // ejecuto la query para obtener todos los registros en un array
+  const movies = query.all(); //Esto es para que nos de todas las peliculas
+
+  res.json({
+    success: true,
+    movies: movies,
+  });
 });
-
-server.get('/movies', (req, res) => {
-	// preparo la query
-	const query = db.prepare('SELECT * FROM movies');
-	// ejecuto la query para obtener todos los registros en un array
-	const movies = query.all();
-
-	res.json({
-		success: true,
-		movies: movies,
-	});
+//Para añadir un usuario con el POST (crear) en el back
+server.post("/signup", (req, res) => {
+  const query = db.prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+  const result = query.run(req.body.email, req.body.password);
+  console.log(result);
+  res.json(result);
 });
+//Para pedir las peliculas de una usuaria en el back
+server.get("/user/movies", (req, res) => {
+  const movieIdsQuery = db.prepare(
+    "SELECT movieId FROM rel_movies_users WHERE userId = ?"
+  );
+  const movieIds = movieIdsQuery.all(req.header("user-id")); //Esto para traer las peliculas favoritas
 
-server.post('/signup', (req, res) => {
-	const query = db.prepare(
-		'INSERT INTO users (email, password) VALUES (?, ?)'
-	);
-	const result = query.run(req.body.email, req.body.password);
-	console.log(result);
-	res.json(result);
-});
-
-server.get('/user/movies', (req, res) => {
-	const movieIdsQuery = db.prepare(
-		'SELECT movieId FROM rel_movies_users WHERE userId = ?'
-	);
-	const movieIds = movieIdsQuery.all(req.header('user-id'));
-
-	console.log(movieIds);
-	res.json({
-		success: true,
-		movies: [],
-	});
+  console.log(movieIds);
+  res.json({
+    success: true,
+    movies: [],
+  });
 });
 
-const staticServerPathWeb = './src/public-react/'; // En esta carpeta ponemos los ficheros estáticos
-server.use(express.static(staticServerPathWeb));
+const staticServerPathWeb = "./src/public-react/"; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb)); //Servidor estatico
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,17 +1,17 @@
 // login
 
 const getMoviesFromApi = () => {
-	console.log('Se están pidiendo las películas de la app');
-	// CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-	return fetch('http://localhost:4001/movies', { method: 'GET' })
-		.then((response) => response.json())
-		.then((data) => {
-			return data;
-		});
+  console.log("Se están pidiendo las películas de la app");
+  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
+  return fetch("http://localhost:4001/movies", { method: "GET" })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    }); // Fetch para pedir todas las peliculas
 };
 
 const objToExport = {
-	getMoviesFromApi: getMoviesFromApi,
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
